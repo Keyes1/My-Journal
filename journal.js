a5373eb158c0cbeea8317a3cfcc52ab02a58c30f
Aanand Deva Suresh <122597947+Keyes1@users.noreply.github.com>
2023-11-09 21:56:09 +0530

    Update journal.js

--- journal.js
+++ journal.js
@@ -1,21 +1,21 @@
-/**
- * Shows the form to add a new entry.
+/*
+ Shows the form to add a new entry.
  */
 function newEntry() {
     document.getElementById('new-entry').classList.remove('hidden');
     document.getElementById('entries').classList.add('hidden');
 }
 
-/**
- * Shows the list of entries.
+/*
+  Shows the list of entries.
  */
 function viewEntries() {
     document.getElementById('new-entry').classList.add('hidden');
     document.getElementById('entries').classList.remove('hidden');
 }
 
-/**
- * Saves the current entry in the form to the list of entries.
+/*
+  Saves the current entry in the form to the list of entries.
  */
 function saveEntry() {
     let date = document.getElementById('date').value;
